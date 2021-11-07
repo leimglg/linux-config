a13a913e99233ae3a4570090ca950903c55491a7
leimglg <80228458+leimglg@users.noreply.github.com>
2021-11-06 20:30:51 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,4 +2,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt install neovim fzf rg
 sudo apt install julia 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# install vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# if using windows, run:  iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
