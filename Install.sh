c5102bc52168aa1d97b392a8e7bb63b28ed7590d
leimglg <80228458+leimglg@users.noreply.github.com>
2021-11-06 20:25:14 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install neovim fzf rg
+sudo apt install julia 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
