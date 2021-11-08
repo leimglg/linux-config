40d31d0553de98205c9a07aadbd009ea8684bfa7
leimglg <80228458+leimglg@users.noreply.github.com>
2021-11-07 20:50:44 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,8 +1,9 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install neovim fzf rg tmux
+sudo apt install neovim fzf rg tmux python3-pip
 sudo apt install julia 
 # install vim-plug for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # if using windows, run:  iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
+pip3 install pynvim
